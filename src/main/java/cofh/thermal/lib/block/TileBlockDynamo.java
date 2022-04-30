31ea1813b978f4d473e72c0bbc8ab4b927759e3c
King Lemming <kinglemming@gmail.com>
2022-04-29 21:35:46 -0700

    Add Waterlogging to dynamos

--- src/main/java/cofh/thermal/lib/block/TileBlockDynamo.java
+++ src/main/java/cofh/thermal/lib/block/TileBlockDynamo.java
@@ -4,17 +4,29 @@ import cofh.core.block.TileBlockActive6Way;
 import cofh.core.tileentity.TileCoFH;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.IWaterLoggable;
+import net.minecraft.fluid.FluidState;
+import net.minecraft.fluid.Fluids;
+import net.minecraft.item.BlockItemUseContext;
+import net.minecraft.state.BooleanProperty;
+import net.minecraft.state.StateContainer;
+import net.minecraft.state.properties.BlockStateProperties;
+import net.minecraft.util.Direction;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.shapes.ISelectionContext;
 import net.minecraft.util.math.shapes.VoxelShape;
 import net.minecraft.util.math.shapes.VoxelShapes;
 import net.minecraft.world.IBlockReader;
+import net.minecraft.world.IWorld;
 
+import javax.annotation.Nullable;
 import java.util.function.Supplier;
 
 import static cofh.lib.util.constants.Constants.FACING_ALL;
 
-public class TileBlockDynamo extends TileBlockActive6Way {
+public class TileBlockDynamo extends TileBlockActive6Way implements IWaterLoggable {
+
+    public static final BooleanProperty WATERLOGGED = BlockStateProperties.WATERLOGGED;
 
     private static final VoxelShape[] BASE_SHAPE = new VoxelShape[]{
             Block.box(0, 6, 0, 16, 16, 16),
@@ -49,9 +61,39 @@ public class TileBlockDynamo extends TileBlockActive6Way {
     }
 
     @Override
+    protected void createBlockStateDefinition(StateContainer.Builder<Block, BlockState> builder) {
+
+        super.createBlockStateDefinition(builder);
+        builder.add(WATERLOGGED);
+    }
+
+    @Override
     public VoxelShape getShape(BlockState state, IBlockReader worldIn, BlockPos pos, ISelectionContext context) {
 
         return DYNAMO_SHAPE[state.getValue(FACING_ALL).ordinal()];
+    }
+
+    @Override
+    public FluidState getFluidState(BlockState state) {
+
+        return state.getValue(WATERLOGGED) ? Fluids.WATER.getSource(false) : super.getFluidState(state);
+    }
+
+    @Override
+    @Nullable
+    public BlockState getStateForPlacement(BlockItemUseContext context) {
+
+        boolean flag = context.getLevel().getFluidState(context.getClickedPos()).getType() == Fluids.WATER;
+        return super.getStateForPlacement(context).setValue(WATERLOGGED, flag);
+    }
+
+    @Override
+    public BlockState updateShape(BlockState stateIn, Direction facing, BlockState facingState, IWorld worldIn, BlockPos currentPos, BlockPos facingPos) {
+
+        if (stateIn.getValue(WATERLOGGED)) {
+            worldIn.getLiquidTicks().scheduleTick(currentPos, Fluids.WATER, Fluids.WATER.getTickDelay(worldIn));
+        }
+        return super.updateShape(stateIn, facing, facingState, worldIn, currentPos, facingPos);
     }
 
 }
